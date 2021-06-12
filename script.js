b7ca2daacd04431a050c1d67d455f7de54d0c080
Anurag Singh <as040999@gmail.com>
2021-06-12 23:29:45 +0530

    made few changes in script.js file

--- script.js
+++ script.js
@@ -4,17 +4,11 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 	h3.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
-h3.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+h3.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
